71804c83fdedbf1c52f5e9ff96fc7b3afba45bab
Granth Sidhant <sidhant2granth@gmail.com>
2026-03-29 17:11:55 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,18 @@
+# i = 0;
+# while i <3:
+#     print("Meow");  
+#     i += 1;
+
+# for _ in range(3):
+#     print("Meow");
+
+# print("Meow \n" * 3);
+# print("Meow \n" * 3, end="");
+
+while True :
+    n = int(input("Enter a number:"))
+    if n > 0:
+        break;
+
+for _ in range(n):
+    print("Meow");
