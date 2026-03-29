97e12734f142f820d49dec6eabbe2f35da532ccb
Granth Sidhant <sidhant2granth@gmail.com>
2026-03-30 00:30:43 +0530

    dict

--- listDict.py
+++ listDict.py
@@ -3,5 +3,10 @@ Students = ["Granth", "Sophie", "Emily", "Bob", "Alice"]
 # for student in Students:
 #     print(student)
 
-for i in range(len(Students)):
-    print(i + 1, Students[i]);
+# for i in range(len(Students)):
+#     print(i + 1, Students[i]);
+
+Student_marks = {"Granth": 85, "Sophie": 92, "Emily": 78, "Bob": 90, "Alice": 88}
+
+for student in Student_marks:
+    print(student, Student_marks[student], sep=": ")
