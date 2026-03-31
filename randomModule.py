f4bfa110c57cc31ac78d2b2b9d913352debd7cd2
Granth Sidhant <sidhant2granth@gmail.com>
2026-03-31 20:30:09 +0530

    rand_int & shuffle

--- randomModule.py
+++ randomModule.py
@@ -1,4 +1,17 @@
 import random
 
-coin_toss = random.choice(["heads", "tail"])
-print(coin_toss)
+# coin_toss = random.choice(["heads", "tail"])
+# print(coin_toss)
+
+# from random import choice
+# coin_toss = choice(["heads", "tail"])
+# print(coin_toss)
+
+# rand_int = random.randint(1, 10)
+# print(rand_int)
+
+cards = ["Queen" , "King" , "Jack" , "Ace"]
+random.shuffle(cards)
+
+for card in cards:
+   print(card)
