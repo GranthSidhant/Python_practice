95250ca3735a7daf7d8c94e64aac267788ce6c2e
Granth Sidhant <sidhant2granth@gmail.com>
2026-03-31 21:47:22 +0530

    command-line arguments better

--- command-line_arguments.py
+++ command-line_arguments.py
@@ -3,3 +3,11 @@ import sys
 # print(" Hello, my name is" , sys.argv[1])
 
 # python .\command-line_arguments.py Granth
+
+if len(sys.argv) < 2:
+    print("Too few arguments")
+
+elif len(sys.argv) > 2:
+        print("Too many arguments")
+
+else: print("Hello my name is", sys.argv[1])
