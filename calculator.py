37a03fcb8b8a69b1aeb67eda90b7ef30362d9de0
Granth Sidhant <sidhant2granth@gmail.com>
2026-04-03 13:56:15 +0530

    pytest

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ def main():
     print(f"The square of {x} is {square(x)}")
 
 def square(n):
-    return n + n
+    return n * n
 
 if __name__ == "__main__":
     main()
--- test_calculator.py
+++ test_calculator.py
@@ -1,25 +1,49 @@
+import pytest
+
 from calculator import square
 
-def main():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("Test failed: square(2) was not 4")
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
 
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("Test failed: square(3) was not 9")
+def test_zero():
+    assert square(0) == 0
 
-    try:
-        assert square(0) == 0
-    except AssertionError:
-        print("Test failed: square(0) was not 0")
+def test_negative():
+    assert square(-1) == 1
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("a string")
+
+
+
+
+
+
+
+# def main():
+#     try:
+#         assert square(2) == 4
+#     except AssertionError:
+#         print("Test failed: square(2) was not 4")
+
+#     try:
+#         assert square(3) == 9
+#     except AssertionError:
+#         print("Test failed: square(3) was not 9")
+
+#     try:
+#         assert square(0) == 0
+#     except AssertionError:
+#         print("Test failed: square(0) was not 0")
         
-    try:
-        assert square(-1) == 1
-    except AssertionError:
-        print("Test failed: square(-1) was not 1")
+#     try:
+#         assert square(-1) == 1
+#     except AssertionError:
+#         print("Test failed: square(-1) was not 1")
 
-    if __name__ == "__main__":
-        main()
+#     if __name__ == "__main__":
+#         main()
