8cf90c947030e9ae1e5ba241c6b7f0f4cf2d20a9
Granth Sidhant <sidhant2granth@gmail.com>
2026-04-03 18:09:31 +0530

    read form file

--- names.py
+++ names.py
@@ -1,5 +1,24 @@
-name = input("What's your name? ");
+# -----------file writing
 
-file = open("name.txt", "a");
-file.write(f"{name} \n");
-file.close();
+# name = input("What's your name? ");
+
+# file = open("name.txt", "a");
+# file.write(f"{name} \n");
+# file.close();
+# ---------------------Better way---------------------
+# with open("name.txt", "a") as file:
+#     file.write(f"{name} \n");
+
+
+# -----------file reading
+
+# with open("name.txt", "r") as file:
+#     lines = file.readlines();
+# for line in lines:
+#     print(f"Hello, {line.strip()}!");
+
+# ----------------------Better way---------------------
+
+with open("name.txt", "r") as file:
+    for line in file:
+        print(f"Hello, {line.strip()}!");
