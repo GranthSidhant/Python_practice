d4c15330db8340745e9d7efa525b69d39e6d981d
Granth Sidhant <sidhant2granth@gmail.com>
2026-04-03 18:52:54 +0530

    sorted file read in better way

--- names.py
+++ names.py
@@ -19,6 +19,24 @@
 
 # ----------------------Better way---------------------
 
-with open("name.txt", "r") as file:
-    for line in file:
-        print(f"Hello, {line.strip()}!");
+# with open("name.txt", "r") as file:
+#     for line in file:
+#         print(f"Hello, {line.strip()}!");
+
+
+#  ----------------------Sorted printing---------------------
+# names = []
+# with open("name.txt") as file:
+#     for line in file:
+#         names.append(line.strip())
+
+#         names.sort()
+
+#         print("Sorted names:")
+#         for name in names:
+#             print(f"Hello, {name}!")
+
+#  ----------------------Better way---------------------
+with open("name.txt") as file:
+    for line in sorted(file):
+        print(f"Hello, {line.strip()}!")
